cd71cb69b494c9c60763cb104b43268a74749945
tangmingze <17853537280@163.com>
2020-04-14 16:37:21 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -8,11 +8,8 @@ function resolve(dir) {
 
 const name = defaultSettings.title || 'vue Admin Template' // page title
 
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
-// port = 9528 npm run dev OR npm run dev --port = 9528
+
+// 更改端口号 如下命令 port = 9528 npm run dev OR npm run dev --port=8080
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
